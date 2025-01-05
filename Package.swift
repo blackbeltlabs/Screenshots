3606075ca40d48f044d92d6d2e6ee72da3a41792
Yulian Drapailo <ulian.onua@gmail.com>
2025-01-05 20:34:39 +0100

    Support for Swift 6 of Package and Example project

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -15,7 +15,6 @@ let package = Package(
     targets: [
         .target(
             name: "Screenshots",
-            dependencies: [],
-            swiftSettings: [.enableUpcomingFeature("StrictConcurrency")])
+            dependencies: [])
     ]
 )
